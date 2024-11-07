112632796936bbdf4916b2036f3aa0add93caccf
Lucien Cusson-Fradet <lucien.fradet@proton.me>
2024-11-07 18:55:11 -0500

    python server to send OSC to tailscale pi and broken send_osc.ino code

---
+++ arduino_code_tests/send_gyro/build/core/wiring_digital.c.d
@@ -0,0 +1,6 @@
+/home/lucienfradet/Arduino/CART461_PEOPLE_WATCHING/arduino_code_tests/send_gyro/build/core/wiring_digital.c.o: \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_digital.c \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_private.h \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/standard/pins_arduino.h
---
+++ arduino_code_tests/send_gyro/build/libraries/Wire/utility/twi.c.d
@@ -0,0 +1,6 @@
+/home/lucienfradet/Arduino/CART461_PEOPLE_WATCHING/arduino_code_tests/send_gyro/build/libraries/Wire/utility/twi.c.o: \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.c \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/standard/pins_arduino.h \
+ /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.h
