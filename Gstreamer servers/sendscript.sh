c066c0e0471cd4b1074171e9db47c9badf742be6
TheWizDotBiz <romprethomas@gmail.com>
2024-11-23 19:25:19 -0500

    asd

--- Gstreamer servers/sendscript.sh
+++ Gstreamer servers/sendscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace <RECEIVER_IP> with the IP address of the Receiver Pi
-RECEIVER_IP="100.64.244.127"  # Replace with your Receiver Pi's IP address
+RECEIVER_IP="100.122.183.98"  # Replace with your Receiver Pi's IP address, was 100.64.244.127
 
 # Capture video with libcamera-vid and pipe it to GStreamer
 libcamera-vid -t 0 --codec h264 --inline --flush --libav-format mpegts -o - | \
