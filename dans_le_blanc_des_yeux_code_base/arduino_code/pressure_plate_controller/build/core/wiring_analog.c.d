b7c6299431bbb120a6290b88a5f26843628e5e5d
Lucien Cusson-Fradet <lucien.fradet@proton.me>
2025-04-10 12:22:54 -0400

    removing build

--- dans_le_blanc_des_yeux_code_base/arduino_code/pressure_plate_controller/build/core/wiring_analog.c.d
+++
@@ -1,6 +0,0 @@
-/home/lucienfradet/Arduino/CART461_DANS_LE_BLANC_DES_YEUX/dans_le_blanc_des_yeux_code_base/arduino_code/pressure_plate_controller/build/core/wiring_analog.c.o: \
- /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_analog.c \
- /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_private.h \
- /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
- /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
- /home/lucienfradet/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/standard/pins_arduino.h
