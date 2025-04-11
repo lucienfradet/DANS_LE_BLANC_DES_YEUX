6315ced47d35843a1c33a9652e996b41f0176a2b
Lucien Cusson-Fradet <lucien.fradet@proton.me>
2025-04-10 22:17:53 -0400

    debug statement

--- dans_le_blanc_des_yeux_code_base/audio_streamer.py
+++ dans_le_blanc_des_yeux_code_base/audio_streamer.py
@@ -169,7 +169,6 @@ class AudioStreamer:
                 # Exact name matching
                 if device_name == self.personal_mic_name:
                     self.personal_mic_id = device_id
-                    print(f"setting id {self.personal_mic_id}")
                     print(f"    → Matched as personal mic (exact match)")
                 
                 if device_name == self.global_mic_name:
@@ -201,8 +200,6 @@ class AudioStreamer:
             
             # Set personal mic gain if found
             if self.personal_mic_id:
-
-                print(f"id is now: {self.personal_mic_id}")
                 cmd = ['pactl', 'set-source-volume', self.personal_mic_id, f'{personal_pa_volume}']
                 result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 
