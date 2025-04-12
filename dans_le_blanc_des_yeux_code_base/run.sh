51c50b3bd4a003279a0ce41e2f1050b143662f6f
Lucien Cusson-Fradet <lucien.fradet@proton.me>
2025-04-11 22:54:41 -0400

    reverting controller

--- dans_le_blanc_des_yeux_code_base/run.sh
+++ dans_le_blanc_des_yeux_code_base/run.sh
@@ -387,8 +387,7 @@ if [ $DISABLE_AUDIO -eq 1 ]; then
 fi
 
 if [ $SERVICE_MODE -eq 1 ]; then
-    ARGS="$ARGS"
-    # ARGS="$ARGS --service"
+    ARGS="$ARGS --service"
 fi
 
 if [ $DISABLE_VIDEO -eq 0 ]; then
